ebd87770dc782b9c400b8daeee7e3e357705c1e8
Doa Ibu <doa.ibu0000000@gmail.com>
2023-07-15 13:31:04 +0700

    library0

---
+++ example/library0_example.dart
@@ -0,0 +1,6 @@
+import 'package:library0/library0.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library library0;
+
+export 'package:library0/src/say_hello.dart';
+export 'package:library0/src/customer.dart' show Customer, Category, Product;
